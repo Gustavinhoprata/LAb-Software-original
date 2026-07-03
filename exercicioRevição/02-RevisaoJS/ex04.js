dcd0d0bdfbf6f136efa6c19d80b204f5b11b753e
Gustavinhoprata <gustavo.9604112@aluno.mg.gov.br>
2026-07-03 14:35:37 -0300

    e isso ai

--- exercicioRevição/02-RevisaoJS/ex04.js
+++ exercicioRevição/02-RevisaoJS/ex04.js
@@ -6,3 +6,7 @@
 // nota2
 
 // Calcule a média e mostre no console
+let nota1 =6
+let nota2 =5
+let media =(nota1 + nota2) /2
+console.log(media)
--- exercicioRevição/02-RevisaoJS/ex07.js
+++ exercicioRevição/02-RevisaoJS/ex07.js
@@ -11,3 +11,17 @@
 // >=5 Recuperação
 
 // Abaixo de 5 Reprovado
+let nota = 9
+if(nota >=9){
+    console.log(Excelente)
+}
+else if (nota >=7){
+    console.log(Bom)
+}
+else if (nota >=5){
+    console.log(Recuperação)
+}
+else {
+   console.log(Reprovado)
+}
+    
